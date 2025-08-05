d8c8f8bbe3b7ede4d6440ffd094891d357cb36ba
Rameez Knan <victorrono65@gmail.com>
2025-08-05 18:28:38 +0300

    Update cloudinary.config.ts

--- src/core/cloudinary/cloudinary.config.ts
+++ src/core/cloudinary/cloudinary.config.ts
@@ -1,4 +1,8 @@
-export const CloudinaryConfig = {
-    apiKey: '2u10LWkyBPYnqYmwPsca83I4wWY',
-    baseURL: `CLOUDINARY_URL=cloudinary://<your_api_key>:<your_api_secret>@dsux8zdf4`,
-}
+
+const CloudinaryConfig = {
+    cloudName: 'dsux8zdf4',       // e.g., 'mycloud123'
+    apiSecret: '2u10LWkyBPYnqYmwPsca83I4wWY',             // e.g., '123456789012345'
+    apiKey: '814375184781759',       // e.g., 'a1b2c3d4e5f6g7h8i9'
+    uploadPreset: 'ai_plant_care', // Required for unsigned uploads (client-side)
+    secure: true                        // Optional, usually true by default
+};
